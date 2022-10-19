4633104ed30b0ff2f46e40a36b2265655bf084f3
CaptainBerryWorld <94391454+Fuad2Dev@users.noreply.github.com>
2022-10-19 22:43:28 +0000

    0x02 update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Print "Holberton"
+ * main - Print "_putchar"
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i = 0;
-	char name_of_school[] = "Holberton";
+	char name_of_school[] = "_putchar";
 
 	while (i < 9)
 	{

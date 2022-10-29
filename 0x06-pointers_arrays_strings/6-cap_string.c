5159f5e5b877c6088137bcb52eb284ac8af2e68d
CaptainBerryWorld <94391454+Fuad2Dev@users.noreply.github.com>
2022-10-29 11:19:14 +0000

    0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,81 +1,32 @@
-#include "main.h"                                                                                                                     
-                                                                                                                                      
-/**                                                                                                                                   
- * cap_string - Capitalizes all words of a string.                                                                                    
- * @str: The string to be capitalized.                                                                                                
- *                                                                                                                                    
- * Return: A pointer to the changed string.                                                                                           
- */                                                                                                                                   
-char *cap_string(char *str)                                                                                                           
-{                                                                                                                                     
-        int index = 0;                                                                                                                
-                                                                                                                                      
-        while (str[index])                                                                                                            
-        {                                                                                                                             
-                while (!(str[index] >= 'a' && str[index] <= 'z'))                                                                     
-                        index++;                                                                                                      
-                                                                                                                                      
-                if (str[index - 1] == ' ' ||                                                                                          
-                    str[index - 1] == '\t' ||                                                                                         
-                    str[index - 1] == '\n' ||                                                                                         
-                    str[index - 1] == ',' ||                                                                                          
-                    str[index - 1] == ';' ||                                                                                          
-                    str[index - 1] == '.' ||                                                                                          
-                    str[index - 1] == '!' ||                                                                                          
-                    str[index - 1] == '?' ||                                                                                          
-                    str[index - 1] == '"' ||                                                                                          
-                    str[index - 1] == '(' ||                                                                                          
-                    str[index - 1] == ')' ||                                                                                          
-                    str[index - 1] == '{' ||                                                                                          
-                    str[index - 1] == '}' ||                                                                                          
-                    index == 0)                                                                                                       
-                        str[index] -= 32;                                                                                             
-                                                                                                                                      
-                index++;                                                                                                              
-        }                                                                                                                             
-                                                                                                                                      
-        return (str);                                                                                                                 
+#include "holberton.h"
+/**
+ *cap_string- converts small letters to cap
+ *@s: checked
+ *Return: s
+ */
+char *cap_string(char *s)
+{
+	int i, j, separatorFound;
+
+	separatorFound = 1;
+
+	for (i = 0; s[i]; ++i)
+	{
+		if (separatorFound && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] -  32;
+		}
+		separatorFound = 0;
+		for (j = 0; j < 12; j++)
+		{
+			if (s[i] == '\t' || s[i] == '\n' || s[i] == ','
+			    || s[i] == '\"' || s[i] == '.' || s[i] == '!' ||
+			    s[i] == '{' || s[i] == '}' || s[i] == '(' ||
+			    s[i] == ')' || s[i] == ' ' || s[i] == '?')
+			{
+				separatorFound = 1;
+			}
+		}
+	}
+	return (s);
 }
-
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-6-cap_string.c[+] [unix] (11:02 29/10/2022)                              27,4-25 Bot
--- INSERT --
-

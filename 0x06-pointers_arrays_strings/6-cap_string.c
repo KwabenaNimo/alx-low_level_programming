d2e3cd3a9e18ced4ccb512742a7294e6cf41696c
CaptainBerryWorld <94391454+Fuad2Dev@users.noreply.github.com>
2022-10-29 11:13:53 +0000

    0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,81 @@
-#include "main.h"
-
-/**
- * *cap_string - capitalizes all words of a string.
- *
- * @str: hold character.
- * Return: string.
- */
-char *cap_string(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i == 0)
-		{
-			if ((str[i] >= 97 && str[i] <= 122))
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else if (str[i] == ' ')
-		{
-			++i;
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 65 && str[i] <= 90)
-			{
-				str[i] = str[i] + 32;
-			}
-		}
-	}
-	return (str);
+#include "main.h"                                                                                                                     
+                                                                                                                                      
+/**                                                                                                                                   
+ * cap_string - Capitalizes all words of a string.                                                                                    
+ * @str: The string to be capitalized.                                                                                                
+ *                                                                                                                                    
+ * Return: A pointer to the changed string.                                                                                           
+ */                                                                                                                                   
+char *cap_string(char *str)                                                                                                           
+{                                                                                                                                     
+        int index = 0;                                                                                                                
+                                                                                                                                      
+        while (str[index])                                                                                                            
+        {                                                                                                                             
+                while (!(str[index] >= 'a' && str[index] <= 'z'))                                                                     
+                        index++;                                                                                                      
+                                                                                                                                      
+                if (str[index - 1] == ' ' ||                                                                                          
+                    str[index - 1] == '\t' ||                                                                                         
+                    str[index - 1] == '\n' ||                                                                                         
+                    str[index - 1] == ',' ||                                                                                          
+                    str[index - 1] == ';' ||                                                                                          
+                    str[index - 1] == '.' ||                                                                                          
+                    str[index - 1] == '!' ||                                                                                          
+                    str[index - 1] == '?' ||                                                                                          
+                    str[index - 1] == '"' ||                                                                                          
+                    str[index - 1] == '(' ||                                                                                          
+                    str[index - 1] == ')' ||                                                                                          
+                    str[index - 1] == '{' ||                                                                                          
+                    str[index - 1] == '}' ||                                                                                          
+                    index == 0)                                                                                                       
+                        str[index] -= 32;                                                                                             
+                                                                                                                                      
+                index++;                                                                                                              
+        }                                                                                                                             
+                                                                                                                                      
+        return (str);                                                                                                                 
 }
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+6-cap_string.c[+] [unix] (11:02 29/10/2022)                              27,4-25 Bot
+-- INSERT --
+
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,37 +1,28 @@
-#include "main.h"
-
-/**
- * *leet - check the code for Holberton School students.
- *
- * @str: string.
- * Return: string.
- */
-char *leet(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == 'a' || str[i] == 'A')
-		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
-		}
-	}
-	return (str);
+#include "main.h"                                                                                                                     
+                                                                                                                                      
+/**                                                                                                                                   
+ * leet - Encodes a string to 1337.                                                                                                   
+ * @str: The string to be encoded.                                                                                                    
+ *                                                                                                                                    
+ * Return: A pointer to the encoded string.                                                                                           
+ */                                                                                                                                   
+char *leet(char *str)                                                                                                                 
+{                                                                                                                                     
+        int indx1 = 0, indx2;                                                                                                         
+        char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};                                                                      
+                                                                                                                                      
+        while (str[indx1])                                                                                                            
+        {                                                                                                                             
+                for (indx2 = 0; indx2 <= 7; indx2++)                                                                                  
+                {                                                                                                                     
+                        if (str[indx1] == leet[indx2] ||                                                                              
+                            str[indx1] - 32 == leet[indx2])                                                                           
+                                str[indx1] = indx2 + '0';                                                                             
+                }                                                                                                                     
+                                                                                                                                      
+                indx1++;                                                                                                              
+        }                                                                                                                             
+                                                                                                                                      
+        return (str);                                                                                                                 
 }
+

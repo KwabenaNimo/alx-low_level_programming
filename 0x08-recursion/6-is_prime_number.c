a4efc66e43265133cea05b7dd6353f38773f2c11
CaptainBerryWorld <86696158+CaptainBerryWorld@users.noreply.github.com>
2022-11-02 08:46:49 +0000

    0x08

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,18 @@
 #include "main.h"
 
+/*
+ * evalute_num - recursioh loop 
+ * @num: num
+ * @iterator: number to iterate
+ * Return: return 1 or 0
+ * */
+
 int evaluate_num(int num, int iterator)
 {
+
 if (iterator == num -1)
-{return (1);
+{
+	return (1);
 }
 
 else if (num % iterator == 0)
@@ -15,15 +24,25 @@ if (num % iterator != 0)
 {
 	return (evaluate_num(num, iterator +1));
 }
+
 return (0);
 
 }
 
+/*
+ * is_prime_number -evaluate prime or not
+ * @num: number
+ * Return: return 1 prime -return 0 otherwise
+ */
+
 int is_prime_number(int num)
 {
+
 	int iterator;
+
 	iterator = 2;
 
+	/* only greater than 2*/
 	if (num < 2)
 	{
 		return (0);
@@ -33,5 +52,6 @@ int is_prime_number(int num)
 	{
 		return (1);
 	}
+
 	return (evaluate_num(num, iterator));
 }
